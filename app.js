0b097025066008ed6710741610254d291d5bdafa
komori <you@example.com>
2026-02-02 13:09:19 +0900

    random

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catRouter = require('./routes/cat');
+var randomRouter = require('./routes/random');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cat',catRouter);
+app.use('/random',randomRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
