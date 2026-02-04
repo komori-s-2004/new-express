b22482490024c1d20ec66edfe5190cbe61cc6120
komori <you@example.com>
2026-02-04 14:47:01 +0900

    report

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catRouter = require('./routes/cat');
 var randomRouter = require('./routes/random');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cat',catRouter);
 app.use('/random',randomRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
